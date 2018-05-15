ad7668b38f398a96c7b14cb8fb3b88a4dafad508
lsminter <lsminteris@gmail.com>
2018-05-15 10:49:50 -0700

    added lesson 2 files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { PeopleService } from './people.service';
 
 @Component({
   selector: 'app-root',
   template: `
     <h1>Angular Services</h1>
+    <pre>{{ people | json }}</pre>
   `
 })
 export class AppComponent {
-  title = 'app';
+  people;
+  constructor() {
+    const peopleService = new PeopleService();
+    this.people = peopleService.getPeople();
+  }
 }
